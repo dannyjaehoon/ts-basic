5bacc62783c70d4ffd1397c8d458fe90dc46f535
dannyjaehoon <jaelee9212@gmail.com>
2021-08-02 16:05:17 +0900

    learned the basic types

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('test');
+var add = function (n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    showResult && console.log(phrase + result);
+    return;
+    return result;
+};
+var number1 = 5;
+var number2 = 2.8;
+var printResult = false;
+var resultPhrase = 'Result is: ';
+add(number1, number2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-console.log('test');
+const add = (n1: number, n2: number, showResult: boolean, phrase: string)=> {
+    const result = n1 + n2;
+   
+
+    return result;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = false;
+const resultPhrase = 'Result is: '
+add(number1, number2, printResult, resultPhrase);
